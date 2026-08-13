713f36bc6c1fff30c2136c40a27b05302ecbc7b2
JacRob32 <jacobrobertsonpdx@gmail.com>
2026-08-13 10:35:29 -0700

    Added Tauri Files

---
+++ src-tauri/target/debug/deps/tauri_plugin_store-fcd449a92f274d90.d
@@ -0,0 +1,9 @@
+/Users/jacobrobertson/Desktop/Coding/Printables Offline/src-tauri/target/debug/deps/tauri_plugin_store-fcd449a92f274d90.d: /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/lib.rs /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/error.rs /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/store.rs
+
+/Users/jacobrobertson/Desktop/Coding/Printables Offline/src-tauri/target/debug/deps/libtauri_plugin_store-fcd449a92f274d90.rlib: /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/lib.rs /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/error.rs /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/store.rs
+
+/Users/jacobrobertson/Desktop/Coding/Printables Offline/src-tauri/target/debug/deps/libtauri_plugin_store-fcd449a92f274d90.rmeta: /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/lib.rs /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/error.rs /Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/store.rs
+
+/Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/lib.rs:
+/Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/error.rs:
+/Users/jacobrobertson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-plugin-store-2.4.4/src/store.rs:
